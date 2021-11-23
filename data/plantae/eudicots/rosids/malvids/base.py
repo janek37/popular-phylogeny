7aca35e87285178e74b416b9e30d305608a33d71
Jan Szejko <janek37@gmail.com>
2021-11-23 20:05:17 +0100

    Add Sapindales

--- data/plantae/eudicots/rosids/malvids/base.py
+++ data/plantae/eudicots/rosids/malvids/base.py
@@ -1,4 +1,16 @@
 from clade import Clade, Species, Genus, Family, Order
 from constants import EN, PL
+from .sapindales import SAPINDALES
 
-MALVIDS = Clade(name='Malvids', children=[])
+P_PELTATUM = Species(
+    name='Pelargonium peltatum',
+    local_names={EN: 'ivy-leaved pelargonium, cascading geranium', PL: 'pelargonia bluszczolistna'},
+)
+
+PELARGONIUM = Genus(name='Pelargonium', children=[P_PELTATUM])
+
+GERANIACEAE = Family(name='Geraniaceae', children=[PELARGONIUM])
+
+GERANIALES = Order(name='Gerniales', children=[GERANIACEAE])
+
+MALVIDS = Clade(name='Malvids', children=[GERANIALES, SAPINDALES])
