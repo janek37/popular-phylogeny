171b07315a0268feba247acf9a8e77cf9f2bb632
Jan Szejko <janek37@gmail.com>
2021-11-28 23:26:24 +0100

    Add Aquifoliales

--- data/plantae/eudicots/asterids/campanulids/base.py
+++ data/plantae/eudicots/asterids/campanulids/base.py
@@ -1,3 +1,18 @@
-from clade import Clade
+from clade import Clade, Family, Genus, Order, Species
+from constants import EN, PL
 
-CAMPANULIDS = Clade(name="campanulids", children=[])
+I_AQUIFOLIUM = Species(
+    name="Ilex aquifolium", local_names={EN: "common holly", PL: "ostrokrzew kolczasty"}
+)
+I_PARAGUARIENSIS = Species(
+    name="Ilex paraguariensis",
+    local_names={EN: "yerba mate", PL: "ostrokrzew paragwajski"},
+)
+
+ILEX = Genus(name="Ilex", children=[I_AQUIFOLIUM, I_PARAGUARIENSIS])
+
+AQUIFOLIACEAE = Family(name="Aquifoliaceae", children=[ILEX])
+
+AQUIFOLIALES = Order(name="Aquifoliales", children=[AQUIFOLIACEAE])
+
+CAMPANULIDS = Clade(name="campanulids", children=[AQUIFOLIALES])
