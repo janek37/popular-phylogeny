c205b2902b75c9e3eab979da64c58905de4f0813
Jan Szejko <janek37@gmail.com>
2021-12-06 01:56:50 +0100

    Add brown algae

--- data/protista.py
+++ data/protista.py
@@ -17,6 +17,11 @@ P_CHROMATOPHORA = Species(
     name="Paulinella chromatophora",
     known_for=[{EN: "photosyntetic plastids unrelated to chloroplasts"}],
 )
+F_VESICULOSUS = Species(
+    name="Fucus vesiculosus",
+    local_names={EN: "bladder wrack", PL: "morszczyn pęcherzykowaty"},
+)
+U_PINNATIFIDA = Species(name="Undaria pinnatifida", local_names={EN: "wakame"})
 
 PLASMODIUM = Genus(name="Plasmodium", children=[P_MALARIAE])
 PARAMECIUM = Genus(name="Paramecium", children=[P_CAUDATUM])
@@ -25,6 +30,8 @@ AMOEBA = Genus(name="Amoeba", children=[A_PROTEUS])
 FULIGO = Genus(name="Fuligo", children=[F_SEPTICA])
 PHYSARUM = Genus(name="Physarum", children=[P_POLYCEPHALUM])
 PAULINELLA = Genus(name="Paulinella", children=[P_CHROMATOPHORA])
+FUCUS = Genus(name="Fucus", children=[F_VESICULOSUS])
+UNDARIA = Genus(name="Undaria", children=[U_PINNATIFIDA])
 
 PLASMODIIDAE = Family(name="Plasmodiidae", children=[PLASMODIUM])
 PARAMECIIDAE = Family(name="Parameciidae", children=[PARAMECIUM])
@@ -32,6 +39,8 @@ TRICHOMONADIDAE = Family(name="Trichomonadidae", children=[TRICHOMONAS])
 AMOEBIDAE = Family(name="Amoebidae", children=[AMOEBA])
 PHYSARACEAE = Family(name="Physaraceae", children=[FULIGO, PHYSARUM])
 PAULINELLIDAE = Family(name="Paulinellidae", children=[PAULINELLA])
+FUCACEAE = Family(name="Fucaceae", children=[FUCUS])
+ALARIACEAE = Family(name="Alariaceae", children=[UNDARIA])
 
 HAEMOSPORORIDA = Order(name="Haemospororida", children=[PLASMODIIDAE])
 PENICULIDA = Order(name="Peniculida", children=[PARAMECIIDAE])
@@ -39,19 +48,24 @@ TRICHOMONADIDA = Order(name="Trichomonadida", children=[TRICHOMONADIDAE])
 TUBULINIDA = Order(name="Tubulinida", children=[AMOEBIDAE])
 PHYSARALES = Order(name="Physarales", children=[PHYSARACEAE])
 EUGLYPHIDA = Order(name="Euglyphida", children=[PAULINELLIDAE])
+FUCALES = Order(name="Fucales", children=[FUCACEAE])
+LAMINARIALES = Order(name="Laminariales", children=[ALARIACEAE])
 
 ACONOIDASIDA = Class(name="Aconoidasida", children=[HAEMOSPORORIDA])
 OLIGOHYMENOPHOREA = Class(name="Oligohymenophorea", children=[PENICULIDA])
 TUBULINEA = Class(name="Tubulinea", children=[TUBULINIDA])
 MYXOGASTRIA = Class(name="Myxogastria", children=[PHYSARALES])
 IMBRICATEA = Class(name="Imbricatea", children=[EUGLYPHIDA])
+PHAEOPHYCEAE = Class(name="Phaeophyceae", children=[FUCALES, LAMINARIALES])
 
 CILIOPHORA = Phylum(name="Ciliphora", children=[OLIGOHYMENOPHOREA])
 APICOMPLEXA = Phylum(name="Apicomplexa", children=[ACONOIDASIDA])
 METAMONADA = Phylum(name="Metamonada", children=[TRICHOMONADIDA])
 AMOEBOZOA = Phylum(name="Amoebozoa", children=[TUBULINEA, MYXOGASTRIA])
 CERCOZOA = Phylum(name="Cercozoa", children=[IMBRICATEA])
+OCHROPHYTA = Phylum(name="Ochrophyta", children=[PHAEOPHYCEAE])
 
 ALVEOLATA = Superphylum(name="Alveolata", children=[CILIOPHORA, APICOMPLEXA])
 
-SAR = Clade(name="SAR", children=[ALVEOLATA, CERCOZOA])
+SAR_A = Clade(children=[ALVEOLATA, OCHROPHYTA])
+SAR = Clade(name="SAR", children=[SAR_A, CERCOZOA])
