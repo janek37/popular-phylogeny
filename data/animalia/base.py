f070538274c5d33f760d1c8b239685c689f96a58
Jan Szejko <janek37@gmail.com>
2021-12-12 02:29:37 +0100

    Reorganize base animals

--- data/animalia/base.py
+++ data/animalia/base.py
@@ -15,11 +15,20 @@ from constants import EN, PL
 from .cnidaria import CNIDARIA
 from .mollusca import MOLLUSCA
 
+# region PORIFERA
 A_FISTULARIS = Species(
     name="Aplysina fistularis",
     local_names={EN: "yellow sponge"},
     known_for={EN: "SpongeBob SquarePants"},
 )
+APLYSINA = Genus(name="Aplysina", children=[A_FISTULARIS])
+APLYSINIDAE = Family(name="Aplysinidae", children=[APLYSINA])
+VERONGIDA = Order(name="Verongida", children=[APLYSINIDAE])
+DEMOSPONGIAE = Class(name="Demospongiae", children=[VERONGIDA])
+PORIFERA = Phylum(name="Porifera", children=[DEMOSPONGIAE])
+# endregion PORIFERA
+
+# region PLATYHELIMNTES
 F_HEPATICA = Species(
     name="Fasciola hepatica",
     local_names={EN: "common liver fluke", PL: "motylica wątrobowa"},
@@ -31,6 +40,26 @@ T_SAGINATA = Species(
 T_SOLIUM = Species(
     name="Taenia solium", local_names={EN: "pork tapeworm", PL: "tasiemiec uzbrojony"}
 )
+
+FASCIOLA = Genus(name="Fasciola", children=[F_HEPATICA])
+TAENIA = Genus(name="Taenia", children=[T_SAGINATA, T_SOLIUM])
+
+FASCIOLIDAE = Family(name="Fasciolidae", children=[FASCIOLA])
+TAENIIDAE = Family(name="Taeniidae", children=[TAENIA])
+
+PLAGIORCHIIDA = Order(name="Plagiorchiida", children=[FASCIOLIDAE])
+CYCLOPHYLLIDEA = Order(name="Cyclophyllidea", children=[TAENIIDAE])
+
+DIGENEA = Subclass(name="Digenea", children=[PLAGIORCHIIDA])
+
+TREMATODA = Class(name="Trematoda", children=[DIGENEA])  # not sure about class
+
+CESTODA = Class(name="Cestoda", children=[CYCLOPHYLLIDEA])
+
+PLATYHELMINTHES = Phylum(name="Platyhelminthes", children=[TREMATODA, CESTODA])
+# endregion PLATYHELIMNTES
+
+# region ANNELIDA
 L_TERRESTRIS = Species(
     name="Lumbricus terrestris",
     local_names={EN: "common earthworm", PL: "dżdżownica ziemna"},
@@ -40,34 +69,19 @@ H_MEDICINALIS = Species(
     local_names={EN: "European medicinal leech", PL: "pijawka lekarska"},
 )
 
-APLYSINA = Genus(name="Aplysina", children=[A_FISTULARIS])
-FASCIOLA = Genus(name="Fasciola", children=[F_HEPATICA])
-TAENIA = Genus(name="Taenia", children=[T_SAGINATA, T_SOLIUM])
 LUMBRICUS = Genus(name="Lumbricus", children=[L_TERRESTRIS])
 HIRUDO = Genus(name="Hirudo", children=[H_MEDICINALIS])
 
-APLYSINIDAE = Family(name="Aplysinidae", children=[APLYSINA])
-FASCIOLIDAE = Family(name="Fasciolidae", children=[FASCIOLA])
-TAENIIDAE = Family(name="Taeniidae", children=[TAENIA])
 LUMBRICIDAE = Family(name="Lumbricidae", children=[LUMBRICUS])
 HIRUDINIDAE = Family(name="Hirudinidae", children=[HIRUDO])
 
-VERONGIDA = Order(name="Verongida", children=[APLYSINIDAE])
-PLAGIORCHIIDA = Order(name="Plagiorchiida", children=[FASCIOLIDAE])
-CYCLOPHYLLIDEA = Order(name="Cyclophyllidea", children=[TAENIIDAE])
 OPISTHOPORA = Order(name="Opisthopora", children=[LUMBRICIDAE])
 ARHYNCHOBDELLIDA = Order(name="Arhynchobdellida", children=[HIRUDINIDAE])
 
-DIGENEA = Subclass(name="Digenea", children=[PLAGIORCHIIDA])
-
-DEMOSPONGIAE = Class(name="Demospongiae", children=[VERONGIDA])
-TREMATODA = Class(name="Trematoda", children=[DIGENEA])  # not sure about class
-CESTODA = Class(name="Cestoda", children=[CYCLOPHYLLIDEA])
 CITELLATA = Class(name="Citellata", children=[OPISTHOPORA, ARHYNCHOBDELLIDA])
 
-PORIFERA = Phylum(name="Porifera", children=[DEMOSPONGIAE])
-PLATYHELMINTHES = Phylum(name="Platyhelminthes", children=[TREMATODA, CESTODA])
 ANNELIDA = Phylum(name="Annelida", children=[CITELLATA])
+# endregion ANNELIDA
 
 LOPHOTROCHOZOA = Superphylum(name="Lophotrochozoa", children=[ANNELIDA, MOLLUSCA])
 
