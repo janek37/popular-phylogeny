9506e44a1d7041185765bdca3f160dc9e60978c6
Jan Szejko <janek37@gmail.com>
2021-12-15 01:16:51 +0100

    Add base Arachnomorpha

--- data/animalia/arthropoda/base.py
+++ data/animalia/arthropoda/base.py
@@ -1,4 +1,29 @@
-from clade import Phylum
-from constants import PL
+from clade import Clade, Class, Family, Genus, Order, Phylum, Species, Subphylum
+from constants import EN, PL
 
-ARTHROPODA = Phylum(name="Arthropoda", children=[], local_names={PL: "stawonogi"})
+P_DAVIDIS = Species(
+    name="Paradoxides davidis", known_for=[{EN: "one of the largest trilobites"}]
+)
+L_POLYPHEMUS = Species(
+    name="Limulus polyphemus",
+    local_names={EN: "Atlantic horseshoe crab", PL: "skrzypłocz"},
+)
+
+PARADOXIDES = Genus(name="Paradoxides", children=[P_DAVIDIS])
+LIMULUS = Genus(name="Limulus", children=[L_POLYPHEMUS])
+
+PARADOXIDIDAE = Family(name="Paradoxididae", children=[PARADOXIDES])
+LIMULIDAE = Family(name="Limulidae", children=[LIMULUS])
+
+REDLICHIIDA = Order(name="Redlichiida", children=[PARADOXIDIDAE])
+XIPHOSURA = Order(name="Xiphosura", children=[LIMULIDAE])
+
+TRILOBITA = Class(name="Trilobita", children=[REDLICHIIDA])
+
+CHELICERATA = Subphylum(name="Chelicerata", children=[XIPHOSURA])
+
+ARACHNOMORPHA = Clade(name="Arachnomorpha", children=[TRILOBITA, CHELICERATA])
+
+ARTHROPODA = Phylum(
+    name="Arthropoda", children=[ARACHNOMORPHA], local_names={PL: "stawonogi"}
+)
