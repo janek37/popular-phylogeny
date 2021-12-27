8893dc52b3fba3df0fa2b7a36ba76c1862ed30fd
Jan Szejko <janek37@gmail.com>
2021-12-27 03:37:55 +0100

    Add Phasmatodea

--- data/animalia/arthropoda/insecta/polyneoptera.py
+++ data/animalia/arthropoda/insecta/polyneoptera.py
@@ -12,19 +12,28 @@ M_RELIGIOSA = Species(
     name="Mantis religiosa",
     local_names={EN: "praying mantis, European mantis", PL: "modliszka zwyczajna"},
 )
+S_SIPYLUS = Species(
+    name="Sipyloidea sipylus",
+    local_names={EN: "pink winged stick insect", PL: "patyczak skrzydlaty"},
+    known_for=[{EN: "the most widespread walking stick in the world"}],
+)
 
 FORFICULA = Genus(name="Forficula", children=[F_AURICULARIA])
 MANTIS = Genus(name="Mantis", children=[M_RELIGIOSA])
+SIPYLOIDEA = Genus(name="Sipyloidea", children=[S_SIPYLUS])
 
 FORFICULIDAE = Family(name="Forficulidae", children=[FORFICULA])
 MANTIDAE = Family(name="Mantidae", children=[MANTIS])
+LONCHODIDAE = Family(name="Lonchodidae", children=[SIPYLOIDEA])
 
 DERMAPTERA = Order(name="Dermaptera", children=[FORFICULIDAE])
 MANTODEA = Order(name="Mantodea", children=[MANTIDAE])
+PHASMATODEA = Order(name="Phasmatodea", children=[LONCHODIDAE])
 
 DICTYOPTERA = Superorder(name="Dictyoptera", children=[MANTODEA, BLATTODEA])
 
-POLYNEOPTERA_A = Clade(children=[ORTHOPTERA, DICTYOPTERA])
+POLYNEOPTERA_B = Clade(children=[DICTYOPTERA, PHASMATODEA])
+POLYNEOPTERA_A = Clade(children=[ORTHOPTERA, POLYNEOPTERA_B])
 
 POLYNEOPTERA = Cohort(
     name="Polyneoptera", children=[DERMAPTERA, POLYNEOPTERA_A]
