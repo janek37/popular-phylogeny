04ea5a9cd2f4caf11df2c4d35a419cc3c30bab3d
Jan Szejko <janek37@gmail.com>
2022-01-02 23:02:56 +0100

    Add sandhopper

--- data/animalia/arthropoda/malacostraca.py
+++ data/animalia/arthropoda/malacostraca.py
@@ -19,23 +19,30 @@ B_GIGANTEUS = Species(
 O_ASELLUS = Species(
     name="Oniscus asellus", local_names={EN: "common woodlouse", PL: "stonoga murowa"}
 )
+T_SALTATOR = Species(
+    name="Talitrus saltator",
+    local_names={EN: "common sand hopper", PL: "zmieraczek plażowy"},
+)
 
 ODONTODACTYLUS = Genus(name="Odontodactylus", children=[O_SCYLLARUS])
 EUPHASIA = Genus(name="Euphasia", children=[E_SUPERBA])
 BATHYNOMUS = Genus(name="Bathynomus", children=[B_GIGANTEUS])
 ONISCUS = Genus(name="Oniscus", children=[O_ASELLUS])
+TALITRUS = Genus(name="Talitrus", children=[T_SALTATOR])
 
 ODONTODACTYLIDAE = Family(name="Odontodactylidae", children=[ODONTODACTYLUS])
 EUPHAUSIIDAE = Family(name="Euphausiidae", children=[EUPHASIA])
 CIROLANIDAE = Family(name="Cirolanidae", children=[BATHYNOMUS])
 ONISCIDAE = Family(name="Oniscidae", children=[ONISCUS])
+TALITRIDAE = Family(name="Talitridae", children=[TALITRUS])
 
 STOMATOPODA = Order(name="Stomatopoda", children=[ODONTODACTYLIDAE])
 EUPHAUSIACEA = Order(name="Euphausiacea", children=[EUPHAUSIIDAE])
 ISOPODA = Order(name="Isopoda", children=[CIROLANIDAE, ONISCIDAE])
+AMPHIPODA = Order(name="Amphipoda", children=[TALITRIDAE])
 
 EUCARIDA = Superorder(name="Eucarida", children=[EUPHAUSIACEA, DECAPODA])
-PERACARIDA = Superorder(name="Peracarida", children=[ISOPODA])
+PERACARIDA = Superorder(name="Peracarida", children=[ISOPODA, AMPHIPODA])
 
 HOPLOCARIDA = Subclass(name="Hoplocarida", children=[STOMATOPODA])
 EUMALACOSTRACA = Subclass(name="Eumalacostraca", children=[EUCARIDA, PERACARIDA])
