dcbe060cd20db4bb671f6f25f039377bddf8f907
Jan Szejko <janek37@gmail.com>
2022-01-06 02:35:15 +0100

    Add base Vertebrata

--- data/animalia/base.py
+++ data/animalia/base.py
@@ -7,13 +7,15 @@ from clade import (
     Order,
     Phylum,
     Species,
+    Subphylum,
     Superphylum,
 )
-from constants import EN
+from constants import EN, PL
 
 from .cnidaria import CNIDARIA
 from .echinodermata import ECHINODERMATA
 from .protostomia import PROTOSTOMIA
+from .vertebrata import VERTEBRATA
 
 # region PORIFERA
 A_FISTULARIS = Species(
@@ -28,7 +30,20 @@ DEMOSPONGIAE = Class(name="Demospongiae", children=[VERONGIDA])
 PORIFERA = Phylum(name="Porifera", children=[DEMOSPONGIAE])
 # endregion PORIFERA
 
-DEUTEROSTOMIA = Superphylum(name="Deuterostomia", children=[ECHINODERMATA])
+# region TUNICATA
+C_INTESTINALIS = Genus(
+    name="Ciona intestinalis", local_names={EN: "vase tunicate", PL: "przejrzystka"}
+)
+CIONA = Genus(name="Ciona", children=[C_INTESTINALIS])
+CIONIDAE = Family(name="Cionidae", children=[CIONA])
+ENTEROGONA = Order(name="Enterogona", children=[CIONIDAE])
+ASCIDIACEA = Class(name="Ascidiacea", children=[ENTEROGONA])
+TUNICATA = Subphylum(name="Tunicata", children=[ASCIDIACEA])
+# endregion TUNICATA
+
+CHORDATA = Phylum(name="Chordata", children=[TUNICATA, VERTEBRATA])
+
+DEUTEROSTOMIA = Superphylum(name="Deuterostomia", children=[ECHINODERMATA, CHORDATA])
 
 NEPHROZOA = Clade(name="Nephrozoa", children=[PROTOSTOMIA, DEUTEROSTOMIA])
 PARAHOXOZOA = Clade(name="ParaHoxozoa", children=[CNIDARIA, NEPHROZOA])
---
+++ data/animalia/vertebrata/base.py
@@ -0,0 +1,28 @@
+from clade import Clade, Class, Family, Genus, Order, Species, Subphylum
+from constants import EN, PL
+
+# region AGNATHA
+M_GLUTINOSA = Species(
+    name="Myxine glutinosa",
+    local_names={EN: "Atlantic hagfish", PL: "śluzica pospolita"},
+)
+P_MARINUS = Species(
+    name="Petromyzon marinus", local_names={EN: "sea lamprey", PL: "minóg morski"}
+)
+
+MYXINE = Genus(name="Myxine", children=[M_GLUTINOSA])
+PETROMYZON = Genus(name="Petromyzon", children=[P_MARINUS])
+
+MYXINIDAE = Family(name="Myxinidae", children=[MYXINE])
+PETROMYZONTIDAE = Family(name="Petromyzontidae", children=[PETROMYZON])
+
+MYXINIFORMES = Order(name="Myxiniformes", children=[MYXINIDAE])
+PETROMYZONTIFORMES = Order(name="Petromyzontiformes", children=[PETROMYZONTIDAE])
+
+MYXINI = Class(name="Myxini", children=[MYXINIFORMES])
+HYPEROARTIA = Class(name="Hyperoartia", children=[PETROMYZONTIFORMES])
+
+AGNATHA = Clade(name="Agnatha", children=[MYXINI, HYPEROARTIA])  # Infraphylum
+# endregion AGNATHA
+
+VERTEBRATA = Subphylum(name="Vertebrata", children=[AGNATHA])
