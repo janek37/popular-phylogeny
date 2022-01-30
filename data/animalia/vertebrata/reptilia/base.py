2408ba6c4b3dafbad8053a8a5ed0c2d722b40b3b
Jan Szejko <janek37@gmail.com>
2022-01-30 21:44:24 +0100

    Add Ichthyosaurus

--- data/animalia/vertebrata/reptilia/base.py
+++ data/animalia/vertebrata/reptilia/base.py
@@ -7,13 +7,18 @@ S_PUNCTATUS = Species(
     name="Sphenodon punctatus",
     local_names={EN: "tuatara", PL: "hatteria, tuatara, łupkoząb"},
 )
+I_COMMUNIS = Species(name="Ichthyosaurus communis")
 
 SPHENODON = Genus(name="Sphenodon", children=[S_PUNCTATUS])
+ICHTHYOSAURUS = Genus(name="Ichthyosaurus", children=[I_COMMUNIS])
 
 SPHENODONTIDAE = Family(name="Sphenodontidae", children=[SPHENODON])
+ICHTHYOSAURIDAE = Family(name="Ichthyosauridae", children=[ICHTHYOSAURUS])
 
 RHYNCHOCEPHALIA = Order(name="Rhynchocephalia", children=[SPHENODONTIDAE])
+ICHTHYOSAURIA = Order(name="Ichthyosauria", children=[ICHTHYOSAURIDAE])
 
 LEPIDOSAURIA = Superorder(name="Lepidosauria", children=[RHYNCHOCEPHALIA, SQUAMATA])
+ICHTHYOPTERYGIA = Superorder(name="Ichthyopterygia", children=[ICHTHYOSAURIA])
 
-REPTILIA = Class(name="Reptilia", children=[LEPIDOSAURIA])
+REPTILIA = Class(name="Reptilia", children=[LEPIDOSAURIA, ICHTHYOPTERYGIA])
