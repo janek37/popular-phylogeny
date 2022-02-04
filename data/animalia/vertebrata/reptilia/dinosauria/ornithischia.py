b6fbf38ec2b5158c20e257f3cfe5a60d33ebbf28
Jan Szejko <janek37@gmail.com>
2022-02-04 20:49:09 +0100

    Add more Ornithischia

--- data/animalia/vertebrata/reptilia/dinosauria/ornithischia.py
+++ data/animalia/vertebrata/reptilia/dinosauria/ornithischia.py
@@ -1,4 +1,4 @@
-from clade import Clade, Family, Genus, Order, Species, Suborder
+from clade import Clade, Family, Genus, Order, Species, Subfamily, Suborder
 
 S_STENOPS = Species(name="Stegosaurus stenops")
 A_MAGNIVENTRIS = Species(name="Ankylosaurus magniventris")
@@ -7,6 +7,9 @@ T_HORRIDUS = Species(name="Triceratops horridus")
 S_ALBERTENSIS = Species(name="Styracosaurus albertensis")
 I_BERNISSARTENSIS = Species(name="Iguanodon bernissartensis")
 S_OSBORNI = Species(name="Saurolophus osborni")
+P_WALKERI = Species(name="Parasaurolophus walkeri")
+C_CASUARIUS = Species(name="Corythosaurus casuarius")
+P_WYOMINGENSIS = Species(name="Pachycephalosaurus wyomingensis")
 
 STEGOSAURUS = Genus(name="Stegosaurus", children=[S_STENOPS])
 ANKYLOSAURUS = Genus(name="Ankylosaurus", children=[A_MAGNIVENTRIS])
@@ -15,21 +18,38 @@ TRICERATOPS = Genus(name="Triceratops", children=[T_HORRIDUS])
 STYRACOSAURUS = Genus(name="Styracosaurus", children=[S_ALBERTENSIS])
 IGUANODON = Genus(name="Iguanodon", children=[I_BERNISSARTENSIS])
 SAUROLOPHUS = Genus(name="Saurolophus", children=[S_OSBORNI])
+PARASAUROLOPHUS = Genus(name="Parasaurolophus", children=[P_WALKERI])
+CORYTHOSAURUS = Genus(name="Corythosaurus", children=[C_CASUARIUS])
+PACHYCEPHALOSAURUS = Genus(name="Pachycephalosaurus", children=[P_WYOMINGENSIS])
+
+SAUROLOPHINAE = Subfamily(name="Saurolophinae", children=[SAUROLOPHUS])
+LAMBEOSAURINAE = Subfamily(
+    name="Lambeosaurinae", children=[PARASAUROLOPHUS, CORYTHOSAURUS]
+)
 
 STEGOSAURIDAE = Family(name="Stegosauridae", children=[STEGOSAURUS])
 ANKYLOSAURIDAE = Family(name="Ankylosauridae", children=[ANKYLOSAURUS])
 PROTOCERATOPSIDAE = Family(name="Protoceratopsidae", children=[PROTOCERATOPS])
 CERATOPSIDAE = Family(name="Ceratopsidae", children=[TRICERATOPS, STYRACOSAURUS])
 IGUANODONTIDAE = Family(name="Iguanodontidae", children=[IGUANODON])
-HADROSAURIDAE = Family(name="Hadrosauridae", children=[SAUROLOPHUS])
+HADROSAURIDAE = Family(name="Hadrosauridae", children=[SAUROLOPHINAE, LAMBEOSAURINAE])
+PACHYCEPHALOSAURIDAE = Family(
+    name="Pachycephalosauridae", children=[PACHYCEPHALOSAURUS]
+)
 
 STEGOSAURIA = Suborder(name="Stegosauria", children=[STEGOSAURIDAE])
 ANKYLOSAURIA = Suborder(name="Ankylosauria", children=[ANKYLOSAURIDAE])
 CERATOPSIA = Suborder(name="Ceratopsia", children=[PROTOCERATOPSIDAE, CERATOPSIDAE])
+PACHYCEPHALOSAURIA = Suborder(
+    name="Pachycephalosauria", children=[PACHYCEPHALOSAURIDAE]
+)
 
 ORNITHOPODA = Clade(name="Ornithopoda", children=[IGUANODONTIDAE, HADROSAURIDAE])
+MARGINOCEPHALIA = Clade(
+    name="Marginocephalia", children=[CERATOPSIA, PACHYCEPHALOSAURIA]
+)
 
 THYREOPHORA = Clade(name="Thyreophora", children=[STEGOSAURIA, ANKYLOSAURIA])
-CERAPODA = Clade(name="Cerapoda", children=[CERATOPSIA, ORNITHOPODA])
+CERAPODA = Clade(name="Cerapoda", children=[MARGINOCEPHALIA, ORNITHOPODA])
 
 ORNITHISCHIA = Order(name="Ornithischia", children=[THYREOPHORA, CERAPODA])
