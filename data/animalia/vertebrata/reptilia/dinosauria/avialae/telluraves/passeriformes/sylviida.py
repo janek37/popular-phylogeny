f77c317a5ab9f170326b4d6acc9f81cce73567db
Jan Szejko <janek37@gmail.com>
2022-03-17 22:35:27 +0100

    Add African swallow

--- data/animalia/vertebrata/reptilia/dinosauria/avialae/telluraves/passeriformes/sylviida.py
+++ data/animalia/vertebrata/reptilia/dinosauria/avialae/telluraves/passeriformes/sylviida.py
@@ -44,7 +44,15 @@ G_CRISTATA = Species(
 )
 H_RUSTICA = Species(
     name="Hirundo rustica",
-    local_names={EN: "barn swallow", PL: "jaskółka dymówka, dymówka"},
+    local_names={EN: "barn swallow, European swallow", PL: "jaskółka dymówka, dymówka"},
+)
+P_SPILODERA = Species(
+    name="Petrochelidon spilodera",
+    local_names={
+        EN: "African swallow, South African cliff swallow",
+        PL: "jaskółka towarzyska",
+    },
+    known_for=[{EN: "carrying coconuts"}],
 )
 D_URBICUM = Species(
     name="Delichon urbicum",
@@ -66,6 +74,7 @@ ALAUDA = Genus(name="Alauda", children=[A_ARVENSIS])
 EREMOPHILA = Genus(name="Eremophila", children=[E_ALPESTRIS])
 GALERIDA = Genus(name="Galerida", children=[G_CRISTATA])
 HIRUNDO = Genus(name="Hirundo", children=[H_RUSTICA])
+PETROCHELIDON = Genus(name="Petrochelidon", children=[P_SPILODERA])
 DELICHON = Genus(name="Delichon", children=[D_URBICUM])
 AEGITHALOS = Genus(name="Aegithalos", children=[A_CAUDATUS])
 
@@ -77,10 +86,14 @@ PARIDAE_D = Clade(children=[PERIPARUS, PARIDAE_C])
 # https://www.researchgate.net/publication/241692441_Multilocus_phylogeny_of_the_avian_family_Alaudidae_larks_reveals_complex_morphological_evolution_non-monophyletic_genera_and_hidden_species_diversity
 ALAUDIDAE_A = Clade(children=[ALAUDA, GALERIDA])
 
+# https://www.sciencedirect.com/science/article/abs/pii/S1055790304003501
+# https://ars.els-cdn.com/content/image/1-s2.0-S1055790304003501-gr1.jpg
+HIRUNDINIDAE_A = Clade(children=[PETROCHELIDON, DELICHON])
+
 PARIDAE = Family(name="Paridae", children=[PARIDAE_A, PARIDAE_D])
 REMIZIDAE = Family(name="Remizidae", children=[REMIZ])
 ALAUDIDAE = Family(name="Alaudidae", children=[ALAUDIDAE_A, EREMOPHILA])
-HIRUNDINIDAE = Family(name="Hirundinidae", children=[HIRUNDO, DELICHON])
+HIRUNDINIDAE = Family(name="Hirundinidae", children=[HIRUNDO, HIRUNDINIDAE_A])
 AEGITHALIDAE = Family(name="Aegithalidae", children=[AEGITHALOS])
 
 SYLVIIDA_A = Clade(children=[PARIDAE, REMIZIDAE])
